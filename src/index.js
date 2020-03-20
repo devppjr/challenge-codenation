24e20a18760542127d9e9165a9b4f6afecd4a984
Pedro Paulo <pedropaulojr.10@gmail.com>
2020-03-19 23:54:59 -0300

    challenge completed

--- src/index.js
+++ src/index.js
@@ -3,32 +3,98 @@ const MY_TOKEN = "0fb02427366245d2c5b6ac06e2bb89b475d81a2f";
 const request = require('request');
 const fs = require('fs');
 const path = require('path');
+const crypto = require('crypto');
 
-const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f',
+    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-const main = async () => {
+async function main() {
     try {
-        const geraJson = await generateDataRequestFromCodenation();
-        const readJson = await readFile();
-        console.log(geraJson);
+        const buildJson = await generateJson();
+        const readedJson = await readFile();
+
+        const json = JSON.parse(readedJson);
+
+        const decodedPhrase = decodePhrase(json.cifrado, json.numero_casas);
+        json.decifrado = decodedPhrase;
+        json.resumo_criptografico = encryptSha(decodedPhrase);
+        const writedJson = await writeFile(JSON.stringify(json));
+        const newReadedJson = await readFile();
+
+        const statusCode = await submitChallenge();
+
+        // console.log(String(newReadedJson))
+
+
+
+
     } catch (error) {
         console.log(error);
     }
 }
+const submitChallenge = () => {
+    const options = {
+        method: "POST",
+        url: "https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=" + MY_TOKEN,
+        headers: {
+            "Content-Type": "multipart/form-data"
+        },
+        formData: {
+            "answer": fs.createReadStream(path.resolve(__dirname, 'answer.json'))
+        }
+    };
 
-const generateDataRequestFromCodenation = async () => {
-    return await new Promise(resolve =>
-        request(CODENATION_URL + MY_TOKEN)
-            .pipe(fs.createWriteStream(path.resolve(__dirname, 'answer.json')))
-            .on('finish', resolve));
+    request(options, (err, res, body) => {
+        if (err)
+            console.log(err)
+
+        console.log(body);
+    })
+}
+const encryptSha = (phraseToDecode) => {
+    return crypto.createHash('sha1')
+        .update(phraseToDecode)
+        .digest('hex');
 }
 
-const decode = (phrase, numbers) => {
+const generateJson = () => new Promise((resolve, reject) => {
+    request(CODENATION_URL + MY_TOKEN)
+        .on('error', () => {
+            reject();
+        })
+        .pipe(fs.createWriteStream(path.resolve(__dirname, 'answer.json')))
+        .on('finish', () => {
+            resolve();
+        });
+})
+
+const readFile = () => new Promise((resolve, reject) => {
+    fs.readFile(path.resolve(__dirname, 'answer.json'), (err, data) => {
+        if (err) {
+            reject(err);
+        } else {
+            resolve(data);
+        }
+    })
+})
+const writeFile = (content) => new Promise((resolve, reject) => {
+    fs.writeFile(path.resolve(__dirname, 'answer.json'), content, (err, data) => {
+        if (err) {
+            reject(err);
+        } else {
+            resolve(data);
+        }
+    })
+})
+
+const decodePhrase = (phrase, numbers) => {
     let decodedPhrase = '';
     if (phrase.length > 0 && numbers >= 0) {
         for (let i = 0; i < phrase.length; i++) {
             const char = phrase.charAt(i);
-            if (alphabet.includes(char)) {
+
+            if (!alphabet.includes(char)) {
                 decodedPhrase += char;
             } else {
                 decodedPhrase += alphabet[getIndexFromAlphabet(alphabet.indexOf(char), numbers)];
@@ -41,14 +107,6 @@ const decode = (phrase, numbers) => {
     return decodedPhrase;
 }
 
-const readFile = async () => {
-    const readed = await fs.readFile(path.resolve(__dirname, 'answer.json'), (err, data) => {
-        if (err)
-            throw err;
-        return data;
-    })
-    return readed;
-}
 const getIndexFromAlphabet = (index, numbers) => {
     let nextIndex = index + numbers;
 
@@ -58,12 +116,4 @@ const getIndexFromAlphabet = (index, numbers) => {
     return nextIndex;
 }
 
-const writeFile = (content) => {
-    fs.writeFile(path.resolve(__dirname, 'answer.json'), content, function (err) {
-        if (err) throw err;
-    });
-}
-
 main();
-// console.log(readFile());
-// console.log(decode("a z", 25))
